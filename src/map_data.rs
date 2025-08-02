72761511444c15bcfaa53aab1a76e9b61259e4e2
zehreken <zehreken@gmail.com>
2025-08-03 00:33:00 +0200

    Update map_data.rs

--- src/map_data.rs
+++ src/map_data.rs
@@ -1,13 +1,29 @@
 use std::{collections::HashMap, sync::LazyLock};
 
-use macroquad::math::Vec2;
+use macroquad::math::{Vec2, vec2};
 
 pub static ARSTAVIKEN_DATA: LazyLock<HashMap<usize, Vec<Vec2>>> = LazyLock::new(|| {
     let mut map = HashMap::new();
-    map.insert(1, vec![Vec2::new(0.0, 0.0)]);
-    map.insert(2, vec![Vec2::new(0.0, 0.0)]);
-    map.insert(3, vec![Vec2::new(0.0, 0.0)]);
-    map.insert(4, vec![Vec2::new(0.0, 0.0)]);
+    map.insert(
+        1,
+        vec![
+            vec2(4020.0, 2016.0),
+            vec2(2660.0, 1709.0),
+            vec2(1120.0, 1463.0),
+            vec2(1647.0, 790.0),
+        ],
+    );
+    map.insert(
+        2,
+        vec![
+            vec2(4350.0, 1885.0),
+            vec2(3803.0, 1786.0),
+            vec2(971.0, 1219.0),
+            vec2(1230.0, 464.0),
+        ],
+    );
+    map.insert(3, vec![vec2(1933.0, 1309.0)]);
+    map.insert(4, vec![vec2(3711.0, 1705.0), vec2(3477.0, 1661.0)]);
 
     map
 });
